95880f24e03b7c510df9c4755c4b619312760ccf
xtaci <daniel820313@gmail.com>
2016-09-24 18:02:09 +0800

    upd

---
+++ lineno.go
@@ -0,0 +1,37 @@
+package logrushook
+
+import (
+	"fmt"
+	"path"
+	"runtime"
+	"strings"
+
+	"github.com/Sirupsen/logrus"
+)
+
+type LineNoHook struct{}
+
+func (hook LineNoHook) Levels() []logrus.Level {
+	return logrus.AllLevels
+}
+
+func (hook LineNoHook) Fire(entry *logrus.Entry) error {
+	i := 0
+	for {
+		if pc, file, line, ok := runtime.Caller(i); ok {
+			funcName := runtime.FuncForPC(pc).Name()
+			fmt.Println(funcName, i)
+			if !strings.Contains(funcName, "github.com/Sirupsen/logrus") && !strings.Contains(funcName, "github.com/xtaci/logrushooks") {
+				entry.Data["file"] = path.Base(file)
+				entry.Data["func"] = path.Base(funcName)
+				entry.Data["line"] = line
+				return nil
+			}
+			i++
+		} else {
+			break
+		}
+	}
+
+	return nil
+}
